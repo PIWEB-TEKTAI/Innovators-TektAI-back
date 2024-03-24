3b455396256b6ff0261e297a999914ce4377050f
Lina Laroussi <laroussilina056@gmail.com>
2024-03-24 04:35:40 +0100

    integration sprint finish

--- app.js
+++ app.js
@@ -25,16 +25,20 @@ const app = express();
 const server = require('http').createServer(app);
 
 
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
-
+// parse requests of content-type - application/json
+app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173' , 
   credentials: true
+  
 }));
+
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
