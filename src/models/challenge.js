c0d04b23300ddcd144ce542c5007073d1d056b6e
rouaida benrabeh <rouaidabenrabeh2000@gmail.com>
2024-05-02 20:46:24 +0200

    challengeModel

--- src/models/challenge.js
+++ src/models/challenge.js
@@ -1,103 +1,36 @@
 const mongoose = require('mongoose');
+const User = require('../models/User'); 
 
 const ChallengeSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId, 
   title: String,
-
-  description:String,
-
-  problematic:String,
-
-  amount:String,
-
-  visibility:String,
-
+  description: String,
+  price: String,
+  image: String,
   status: {
     type: String,
     enum: ['open', 'completed', 'archived'],
-    default: 'open' // You can set a default value if needed
+    default: 'open'
   },
   startDate: {
     type: Date,
-    required:true
+    default: Date.now
   },
   endDate: {
     type: Date,
     required: true
   },
   createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    type: String,
+    ref: User,
     required: true
   },
-  targetedSkills: [String], 
-  
-  fileUrl:String,
-  
-
-  rankingMode: {
-    automated:Boolean,
-    expert:Boolean,
-  },
-
-  numberParticipants:{
-      nbrTeam:String,
-      nbrSolo:String
-  },
-
-  bareme: {
-    output:Boolean,
-    presentation:Boolean,
-    codeSource:Boolean,
-    dataSet:Boolean,
-    readmeFile:Boolean,
-    rapport:Boolean,
-    Demo:Boolean
-  },
-
-
-  prizes:{
-     prizeName:String,
-     prizeDescription:String
-  },
-
-  recruitement:{
-     positionTitle:String,
-     jobDescription:String
-  },
-  
-  freelance:{
-    projectTitle:String,
-    projectDescription:String
- },
-
-  internship:{
-    internshipTitle:String,
-    internshipDescription:String,
-    duration:String
-  },
-
-  image:String,
-
-  participations: {
-    soloParticipants: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }],
-    soloParticipationRequests: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }],
-    TeamParticipants: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Team'
-    }],
-    TeamParticipationRequests: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Team'
-    }]
-  } 
+  targetedSkills: [String],
+  dataset: [{
+    name: String,
+    description: String,
+    fileUrl: String,
+  }],
 });
-
-
 
 module.exports = mongoose.model('Challenge', ChallengeSchema);
